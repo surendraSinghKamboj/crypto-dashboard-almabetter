863fd8e50315b0b62f1b8bd250385ea5d25fab09
nancykaur11 <nancykaur1119@gmail.com>
2023-05-28 13:57:41 +0530

    NAVBAR SEARCHBAR

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import { Navbar } from "./component/Navbar";
+import { Dropdown } from "./component/Dropdown";
+import { Searchbar } from "./component/Searchbar";
 function App() {
   return (
-    <h1 className="text-3xl text-green-500 font-medium underline">
-      Hello world!
-    </h1>
+    <div>
+      <Searchbar />
+    </div>
   );
 }
 
