6befc3a4e5a1a51d63b92e0435cc22f48a8d7883
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-05-28 20:16:39 +0530

    es lint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import { Navbar } from "./component/Navbar";
 import { Dropdown } from "./component/Dropdown";
 import { Searchbar } from "./component/Searchbar";
